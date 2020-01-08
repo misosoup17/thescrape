5bf181dbabc527738b8c444e6a1e81f7a46f2dde
misosoup17 <misagaspar@icloud.com>
2020-01-07 19:41:52 -0800

    updated

---
+++ models/note.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+var NoteSchema = new Schema({
+  title: String,
+  body: String
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+// Export the Note model
+module.exports = Note;
